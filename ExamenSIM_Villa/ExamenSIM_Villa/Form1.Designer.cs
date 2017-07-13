ecc40a472cefade2970eab4438425133c8d962ab
pablo657 <pablo657@hotmail.com>
2017-07-13 01:45:38 -0300

    VALIDACIONES

--- ExamenSIM_Villa/ExamenSIM_Villa/Form1.Designer.cs
+++ ExamenSIM_Villa/ExamenSIM_Villa/Form1.Designer.cs
@@ -35,16 +35,6 @@
             this.txt_garantia = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.grp_politica_1 = new System.Windows.Forms.GroupBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txt_Costo_reparacion = new System.Windows.Forms.TextBox();
-            this.txt_prom_vida_pol_1 = new System.Windows.Forms.TextBox();
-            this.txt_prom_vida_pol_2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txt_prom_vida_pol_3 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.grilla_1 = new System.Windows.Forms.DataGridView();
             this.nAuto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.rdn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -55,6 +45,9 @@
             this.costProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.costTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.costAcum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txt_prom_vida_pol_1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.grilla_2 = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -65,6 +58,9 @@
             this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txt_prom_vida_pol_2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.grilla_3 = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -75,11 +71,15 @@
             this.dataGridViewTextBoxColumn16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txt_prom_vida_pol_3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_simular = new System.Windows.Forms.Button();
+            this.txt_Costo_reparacion = new System.Windows.Forms.TextBox();
             this.grp_politica_1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grilla_1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grilla_2)).BeginInit();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grilla_3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -99,6 +99,7 @@
             this.txt_cant_prod.Size = new System.Drawing.Size(47, 20);
             this.txt_cant_prod.TabIndex = 1;
             this.txt_cant_prod.Text = "120";
+            this.txt_cant_prod.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_cant_prod_KeyPress);
             // 
             // label2
             // 
@@ -116,15 +117,16 @@
             this.txt_garantia.Size = new System.Drawing.Size(47, 20);
             this.txt_garantia.TabIndex = 3;
             this.txt_garantia.Text = "5";
+            this.txt_garantia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_garantia_KeyPress);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(236, 18);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.Size = new System.Drawing.Size(108, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Costo de areparación=";
+            this.label3.Text = "Costo de reparación=";
             // 
             // grp_politica_1
             // 
@@ -137,100 +139,6 @@
             this.grp_politica_1.TabIndex = 6;
             this.grp_politica_1.TabStop = false;
             this.grp_politica_1.Text = "Politica 1";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.grilla_2);
-            this.groupBox1.Controls.Add(this.txt_prom_vida_pol_2);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Location = new System.Drawing.Point(15, 226);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(954, 179);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Politica 2";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.grilla_3);
-            this.groupBox2.Controls.Add(this.txt_prom_vida_pol_3);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Location = new System.Drawing.Point(15, 411);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(954, 179);
-            this.groupBox2.TabIndex = 7;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Politica 3";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Maroon;
-            this.button1.Location = new System.Drawing.Point(708, 611);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "SIMULAR";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 16);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(138, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Vida promedio en años (m)=";
-            // 
-            // txt_Costo_reparacion
-            // 
-            this.txt_Costo_reparacion.Location = new System.Drawing.Point(356, 15);
-            this.txt_Costo_reparacion.Name = "txt_Costo_reparacion";
-            this.txt_Costo_reparacion.Size = new System.Drawing.Size(47, 20);
-            this.txt_Costo_reparacion.TabIndex = 5;
-            this.txt_Costo_reparacion.Text = "5";
-            // 
-            // txt_prom_vida_pol_1
-            // 
-            this.txt_prom_vida_pol_1.Location = new System.Drawing.Point(150, 13);
-            this.txt_prom_vida_pol_1.Name = "txt_prom_vida_pol_1";
-            this.txt_prom_vida_pol_1.Size = new System.Drawing.Size(47, 20);
-            this.txt_prom_vida_pol_1.TabIndex = 9;
-            this.txt_prom_vida_pol_1.Text = "4";
-            // 
-            // txt_prom_vida_pol_2
-            // 
-            this.txt_prom_vida_pol_2.Location = new System.Drawing.Point(150, 13);
-            this.txt_prom_vida_pol_2.Name = "txt_prom_vida_pol_2";
-            this.txt_prom_vida_pol_2.Size = new System.Drawing.Size(47, 20);
-            this.txt_prom_vida_pol_2.TabIndex = 10;
-            this.txt_prom_vida_pol_2.Text = "5";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 16);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(138, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Vida promedio en años (m)=";
-            // 
-            // txt_prom_vida_pol_3
-            // 
-            this.txt_prom_vida_pol_3.Location = new System.Drawing.Point(150, 13);
-            this.txt_prom_vida_pol_3.Name = "txt_prom_vida_pol_3";
-            this.txt_prom_vida_pol_3.Size = new System.Drawing.Size(47, 20);
-            this.txt_prom_vida_pol_3.TabIndex = 12;
-            this.txt_prom_vida_pol_3.Text = "6";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 16);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(138, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Vida promedio en años (m)=";
             // 
             // grilla_1
             // 
@@ -248,7 +156,7 @@
             this.grilla_1.Location = new System.Drawing.Point(20, 51);
             this.grilla_1.Name = "grilla_1";
             this.grilla_1.RowHeadersVisible = false;
-            this.grilla_1.Size = new System.Drawing.Size(928, 128);
+            this.grilla_1.Size = new System.Drawing.Size(905, 128);
             this.grilla_1.TabIndex = 10;
             // 
             // nAuto
@@ -305,6 +213,36 @@
             this.costAcum.Name = "costAcum";
             this.costAcum.ReadOnly = true;
             // 
+            // txt_prom_vida_pol_1
+            // 
+            this.txt_prom_vida_pol_1.Location = new System.Drawing.Point(150, 13);
+            this.txt_prom_vida_pol_1.Name = "txt_prom_vida_pol_1";
+            this.txt_prom_vida_pol_1.Size = new System.Drawing.Size(47, 20);
+            this.txt_prom_vida_pol_1.TabIndex = 9;
+            this.txt_prom_vida_pol_1.Text = "4";
+            this.txt_prom_vida_pol_1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_prom_vida_pol_1_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Vida promedio en años (m)=";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grilla_2);
+            this.groupBox1.Controls.Add(this.txt_prom_vida_pol_2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(15, 226);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(954, 179);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Politica 2";
+            // 
             // grilla_2
             // 
             this.grilla_2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -321,7 +259,7 @@
             this.grilla_2.Location = new System.Drawing.Point(20, 45);
             this.grilla_2.Name = "grilla_2";
             this.grilla_2.RowHeadersVisible = false;
-            this.grilla_2.Size = new System.Drawing.Size(928, 128);
+            this.grilla_2.Size = new System.Drawing.Size(905, 128);
             this.grilla_2.TabIndex = 12;
             // 
             // dataGridViewTextBoxColumn1
@@ -378,6 +316,36 @@
             this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
             this.dataGridViewTextBoxColumn9.ReadOnly = true;
             // 
+            // txt_prom_vida_pol_2
+            // 
+            this.txt_prom_vida_pol_2.Location = new System.Drawing.Point(150, 13);
+            this.txt_prom_vida_pol_2.Name = "txt_prom_vida_pol_2";
+            this.txt_prom_vida_pol_2.Size = new System.Drawing.Size(47, 20);
+            this.txt_prom_vida_pol_2.TabIndex = 10;
+            this.txt_prom_vida_pol_2.Text = "5";
+            this.txt_prom_vida_pol_2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_prom_vida_pol_2_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Vida promedio en años (m)=";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grilla_3);
+            this.groupBox2.Controls.Add(this.txt_prom_vida_pol_3);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(15, 411);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(954, 179);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Politica 3";
+            // 
             // grilla_3
             // 
             this.grilla_3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -394,7 +362,7 @@
             this.grilla_3.Location = new System.Drawing.Point(20, 39);
             this.grilla_3.Name = "grilla_3";
             this.grilla_3.RowHeadersVisible = false;
-            this.grilla_3.Size = new System.Drawing.Size(928, 128);
+            this.grilla_3.Size = new System.Drawing.Size(905, 128);
             this.grilla_3.TabIndex = 13;
             // 
             // dataGridViewTextBoxColumn10
@@ -451,12 +419,51 @@
             this.dataGridViewTextBoxColumn18.Name = "dataGridViewTextBoxColumn18";
             this.dataGridViewTextBoxColumn18.ReadOnly = true;
             // 
+            // txt_prom_vida_pol_3
+            // 
+            this.txt_prom_vida_pol_3.Location = new System.Drawing.Point(150, 13);
+            this.txt_prom_vida_pol_3.Name = "txt_prom_vida_pol_3";
+            this.txt_prom_vida_pol_3.Size = new System.Drawing.Size(47, 20);
+            this.txt_prom_vida_pol_3.TabIndex = 12;
+            this.txt_prom_vida_pol_3.Text = "6";
+            this.txt_prom_vida_pol_3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_prom_vida_pol_3_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Vida promedio en años (m)=";
+            // 
+            // btn_simular
+            // 
+            this.btn_simular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_simular.ForeColor = System.Drawing.Color.Maroon;
+            this.btn_simular.Location = new System.Drawing.Point(708, 611);
+            this.btn_simular.Name = "btn_simular";
+            this.btn_simular.Size = new System.Drawing.Size(75, 23);
+            this.btn_simular.TabIndex = 8;
+            this.btn_simular.Text = "SIMULAR";
+            this.btn_simular.UseVisualStyleBackColor = true;
+            this.btn_simular.Click += new System.EventHandler(this.btn_simular_Click);
+            // 
+            // txt_Costo_reparacion
+            // 
+            this.txt_Costo_reparacion.Location = new System.Drawing.Point(356, 15);
+            this.txt_Costo_reparacion.Name = "txt_Costo_reparacion";
+            this.txt_Costo_reparacion.Size = new System.Drawing.Size(47, 20);
+            this.txt_Costo_reparacion.TabIndex = 5;
+            this.txt_Costo_reparacion.Text = "5";
+            this.txt_Costo_reparacion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Costo_reparacion_KeyPress);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(988, 646);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_simular);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.grp_politica_1);
@@ -470,12 +477,12 @@
             this.Text = "Final SIM - ej 318";
             this.grp_politica_1.ResumeLayout(false);
             this.grp_politica_1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla_1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grilla_2)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.grilla_1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grilla_2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.grilla_3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -493,7 +500,7 @@
         private System.Windows.Forms.GroupBox grp_politica_1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_simular;
         private System.Windows.Forms.TextBox txt_prom_vida_pol_1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txt_prom_vida_pol_2;
--- ExamenSIM_Villa/ExamenSIM_Villa/Form1.cs
+++ ExamenSIM_Villa/ExamenSIM_Villa/Form1.cs
@@ -15,5 +15,77 @@ namespace ExamenSIM_Villa
         {
             InitializeComponent();
         }
+
+        private void btn_simular_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void validar()
+        {
+
+        }
+
+        private void txt_cant_prod_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar) < 48 && e.KeyChar != 8) || e.KeyChar > 57)
+            {
+                MessageBox.Show("Sólo se permiten Números");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_Costo_reparacion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (((e.KeyChar) < 48 && e.KeyChar != 8) || e.KeyChar > 57)
+            //{
+            //    MessageBox.Show("Sólo se permiten Números");
+            //    e.Handled = true;
+            //}
+
+
+            // ACA PERMITE PONER COMAS, PERO PODES PONER MAS DE UNA ¨OJO¨
+
+            if (((e.KeyChar) < 48 && e.KeyChar != 8 && e.KeyChar != 44) || e.KeyChar > 57)
+            {
+                MessageBox.Show("Sólo se permiten Números");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_garantia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar) < 48 && e.KeyChar != 8) || e.KeyChar > 57)
+            {
+                MessageBox.Show("Sólo se permiten Números");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_prom_vida_pol_1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar) < 48 && e.KeyChar != 8) || e.KeyChar > 57)
+            {
+                MessageBox.Show("Sólo se permiten Números");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_prom_vida_pol_2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar) < 48 && e.KeyChar != 8) || e.KeyChar > 57)
+            {
+                MessageBox.Show("Sólo se permiten Números");
+                e.Handled = true;
+            }
+        }
+
+        private void txt_prom_vida_pol_3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar) < 48 && e.KeyChar != 8) || e.KeyChar > 57)
+            {
+                MessageBox.Show("Sólo se permiten Números");
+                e.Handled = true;
+            }
+        }
     }
 }
